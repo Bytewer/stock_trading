6783ae8b4ea82a3931f0d02e8a74858f1287cdd7
Bytewer <dwx_job@outlook.com>
2024-08-28 23:21:48 +0800

    1

--- app/examples/layout.tsx
+++ app/examples/layout.tsx
@@ -1,15 +1,6 @@
 import { Metadata } from "next";
-import Link from "next/link";
 
-import { cn } from "@/lib/utils";
 import { ExamplesNav } from "@/components/examples-nav";
-import {
-  PageActions,
-  PageHeader,
-  PageHeaderDescription,
-  PageHeaderHeading,
-} from "@/components/page-header";
-import { buttonVariants } from "@/components/ui/button";
 
 export const metadata: Metadata = {
   title: "Examples",
@@ -23,25 +14,6 @@ interface ExamplesLayoutProps {
 export default function ExamplesLayout({ children }: ExamplesLayoutProps) {
   return (
     <div className="container relative">
-      <PageHeader>
-        <PageHeaderHeading className="hidden md:block">Check out some examples</PageHeaderHeading>
-        <PageHeaderHeading className="md:hidden">Examples</PageHeaderHeading>
-        <PageHeaderDescription>
-          Dashboard, cards, authentication. Some examples built using the components. Use this as a
-          guide to build your own.
-        </PageHeaderDescription>
-        <PageActions>
-          <Link href="/docs" className={cn(buttonVariants(), "rounded-[6px]")}>
-            Get Started
-          </Link>
-          <Link
-            href="/components"
-            className={cn(buttonVariants({ variant: "outline" }), "rounded-[6px]")}
-          >
-            Components
-          </Link>
-        </PageActions>
-      </PageHeader>
       <section>
         <ExamplesNav />
         <div className="overflow-hidden rounded-[0.5rem] border bg-background shadow">
