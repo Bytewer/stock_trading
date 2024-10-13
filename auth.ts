c91b830aac0be8e1c3b6491874e11d543dcea977
Bytewer <dwx_job@outlook.com>
2024-10-13 14:26:29 +0800

    1

--- auth.ts
+++ auth.ts
@@ -1,5 +1,42 @@
 import NextAuth from "next-auth";
+import Credentials from "next-auth/providers/credentials";
+// Your own logic for dealing with plaintext password strings; be careful!
+// import { saltAndHashPassword } from "@/utils/password";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [],
+  providers: [
+    Credentials({
+      // You can specify which fields should be submitted, by adding keys to the `credentials` object.
+      // e.g. domain, username, password, 2FA token, etc.
+      credentials: {
+        email: {},
+        password: {},
+      },
+      authorize: async (credentials) => {
+        let user = null;
+
+        // logic to salt and hash password
+        // const pwHash = saltAndHashPassword(credentials.password);
+
+        // logic to verify if the user exists
+        // user = await getUserFromDb(credentials.email, pwHash);
+        //           user = {
+        //               id?: string
+        //   name?: string | null
+        //   email?: string | null
+        //   image?: string | null
+        //           }
+        user = null;
+
+        if (!user) {
+          // No user found, so this is their first attempt to login
+          // meaning this is also the place you could do registration
+          throw new Error("User not found.");
+        }
+
+        // return user object with their profile data
+        return user;
+      },
+    }),
+  ],
 });
