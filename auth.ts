c8d3d23d50f847e0216d05a087c8d4a838f188e1
Bytewer <dwx_job@outlook.com>
2024-10-13 14:37:49 +0800

    1

--- auth.ts
+++ auth.ts
@@ -1,9 +1,12 @@
 import NextAuth from "next-auth";
+import { ZodError } from "zod";
 import Credentials from "next-auth/providers/credentials";
+import { signInSchema } from "./lib/zod";
 // Your own logic for dealing with plaintext password strings; be careful!
 // import { saltAndHashPassword } from "@/utils/password";
+// import { getUserFromDb } from "@/utils/db";
 
-export const { handlers, signIn, signOut, auth } = NextAuth({
+export const { handlers, signIn, auth } = NextAuth({
   providers: [
     Credentials({
       // You can specify which fields should be submitted, by adding keys to the `credentials` object.
@@ -13,29 +16,36 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
         password: {},
       },
       authorize: async (credentials) => {
-        let user = null;
+        try {
+          let user = null;
 
-        // logic to salt and hash password
-        // const pwHash = saltAndHashPassword(credentials.password);
+          const { email, password } = await signInSchema.parseAsync(credentials);
 
-        // logic to verify if the user exists
-        // user = await getUserFromDb(credentials.email, pwHash);
-        //           user = {
-        //               id?: string
-        //   name?: string | null
-        //   email?: string | null
-        //   image?: string | null
-        //           }
-        user = null;
+          // logic to salt and hash password
+          //   const pwHash = saltAndHashPassword(password);
 
-        if (!user) {
-          // No user found, so this is their first attempt to login
-          // meaning this is also the place you could do registration
-          throw new Error("User not found.");
+          // logic to verify if the user exists
+          //   user = await getUserFromDb(email, pwHash);
+          //           user = {
+          //               id?: string
+          //   name?: string | null
+          //   email?: string | null
+          //   image?: string | null
+          //           }
+          //   user = null;
+          user = { id: "we", name: "we", email: "asd@demo.com" };
+          if (!user) {
+            throw new Error("User not found.");
+          }
+
+          // return JSON object with the user data
+          return user;
+        } catch (error) {
+          if (error instanceof ZodError) {
+            // Return `null` to indicate that the credentials are invalid
+            return null;
+          }
         }
-
-        // return user object with their profile data
-        return user;
       },
     }),
   ],
